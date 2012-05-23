8cba7ad84edee605dc161f697f03e930515cba16
Dennis Roberts <dennis@iplantcollaborative.org>
2012-05-23 09:02:49 -0700

    CORE-3200: initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.iplantc/lein-iplant-cmdtar "0.1.0-SNAPSHOT"
+  :eval-in-leiningen true
+  :description "Leiningen plugin for generating tarball command distributions."
+  :url "https://github.com/iPlantCollaborativeOpenSource/lein-iplant-cmdtar"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [fleet "0.9.5"]])
