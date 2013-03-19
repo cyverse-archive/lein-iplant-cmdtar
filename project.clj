4712eeee416722f3e0de242cae8268b32fb8dbcc
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-19 15:43:36 -0700

    upped dependencies and increased version
    clojure -> 1.5.0
    fleet -> 0.10.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.iplantc/lein-iplant-cmdtar "0.1.0-SNAPSHOT"
+(defproject org.iplantc/lein-iplant-cmdtar "0.1.1-SNAPSHOT"
   :eval-in-leiningen true
   :description "Leiningen plugin for generating tarball command distributions."
   :url "https://github.com/iPlantCollaborativeOpenSource/lein-iplant-cmdtar"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [fleet "0.9.5"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [fleet "0.10.1"]])
