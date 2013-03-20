63f0cf0e0f70d29dc973b9012faa9b3eb12f743e
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-20 15:14:08 -0700

    clojure 1.5.1

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :eval-in-leiningen true
   :description "Leiningen plugin for generating tarball command distributions."
   :url "https://github.com/iPlantCollaborativeOpenSource/lein-iplant-cmdtar"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [fleet "0.10.1"]])
