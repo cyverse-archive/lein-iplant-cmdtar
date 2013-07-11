0d9d92035202922084ef843a23946ce7f8d17e66
Dennis Roberts <dennis@iplantcollaborative.org>
2013-07-11 10:35:04 -0700

    CORE-4553: switched back to fleet version 0.9.5

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :description "Leiningen plugin for generating tarball command distributions."
   :url "https://github.com/iPlantCollaborativeOpenSource/lein-iplant-cmdtar"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [fleet "0.10.1"]])
+                 [fleet "0.9.5"]])
