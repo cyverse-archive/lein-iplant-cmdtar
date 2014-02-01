46f90a428f68af5ebe81a28664d776fff26ac0bd
Dennis Roberts <dennis@iplantcollaborative.org>
2014-01-31 17:05:32 -0700

    added some elements that are required by sonatype to project.clj

--- project.clj
+++ project.clj
@@ -2,5 +2,17 @@
   :eval-in-leiningen true
   :description "Leiningen plugin for generating tarball command distributions."
   :url "https://github.com/iPlantCollaborativeOpenSource/lein-iplant-cmdtar"
+  :license {:name "BSD"
+            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/facepalm.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/facepalm.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/facepalm.git"}
+  :pom-addition [:developers
+                 [:developer
+                  [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [fleet "0.9.5"]])
+                 [fleet "0.9.5"]]
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
+                 ["sonatype-nexus-staging"
+                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]])
