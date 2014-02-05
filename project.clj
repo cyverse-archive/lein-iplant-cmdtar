0e5bed323b28f0492419e38a24dfdcbd065e9654
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-04 18:57:47 -0700

    CORE-5133: moved the nexus staging repository to the reploy-repositories field

--- project.clj
+++ project.clj
@@ -13,6 +13,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [fleet "0.9.5"]]
   :repositories [["sonatype-nexus-snapshots"
-                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
-                 ["sonatype-nexus-staging"
-                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]])
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
+  :deploy-repositories [["sonatype-nexus-staging"
+                         {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]])
